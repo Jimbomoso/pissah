f403cb2a74bba3f22a4783ad16555dfef18de9ab
J.C <jimbomoso@gmail.com>
2021-08-20 14:25:28 -0400

    Moves links to footer and updates heading

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,29 +12,31 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Pissah!
-        </h1>
+        <h1 className={styles.title}>Pissah</h1>
 
-        <h2 className={styles.description}>
-          This weeks punishment:
-        </h2>
-
-        <h2 className={styles.description}>
-          Links:
-        </h2>
-        <ul>
-          <li> <a href="https://forms.gle/hEWCH8fZkWUGEo1x7">Submit punishments </a> </li>
-          <li> <a href="https://docs.google.com/spreadsheets/d/1HqBUGAbqIflPSKtJ93eNfqBkUkCzmMyVnv0PcwvXNFw/edit?usp=sharing"> Keepers </a> </li>
-        </ul>
+        <h2 className={styles.description}>This weeks punishment:</h2>
 
         <h3> </h3>
-
       </main>
 
       <footer className={styles.footer}>
-        
+        <h2 className={styles.description}>Links:</h2>
+        <ul>
+          <li>
+            {" "}
+            <a href="https://forms.gle/hEWCH8fZkWUGEo1x7">
+              Submit punishments{" "}
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="https://docs.google.com/spreadsheets/d/1HqBUGAbqIflPSKtJ93eNfqBkUkCzmMyVnv0PcwvXNFw/edit?usp=sharing">
+              {" "}
+              Keepers{" "}
+            </a>{" "}
+          </li>
+        </ul>
       </footer>
     </div>
-  )
+  );
 }
