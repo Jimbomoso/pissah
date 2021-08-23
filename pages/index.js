f3dd6513f51fd492597a456e8b0f9a4e746f69d8
J.C <jimbomoso@gmail.com>
2021-08-23 18:01:12 -0400

    Styles and embeds sheet

--- pages/index.js
+++ pages/index.js
@@ -4,39 +4,42 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Pissah</title>
-        <meta name="description" content="Pissah" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Pissah</title>
+          <meta name="description" content="Pissah" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        
+        <main className={styles.main}>
         <h1 className={styles.title}>Pissah</h1>
+          <h2 className={styles.description}>This week's punishment:</h2>
+          <iframe
+            frameBorder="0"
+            src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQMODec2Pd5vr7lKJZsUHSHqMj6upRG3v5jCZHJUaCxten2YQyalh-Gef_qB1bgRL2dNALMVOplMeB5/pubhtml?gid=1392528245&amp;single=true&amp;widget=false&amp;headers=false&amp;chrome=false&amp;"
+          ></iframe>
+        </main>
 
-        <h2 className={styles.description}>This weeks punishment:</h2>
-
-        <h3> </h3>
-      </main>
-
-      <footer className={styles.footer}>
-        <h2 className={styles.description}>Links:</h2>
-        <ul>
-          <li>
-            {" "}
-            <a href="https://forms.gle/hEWCH8fZkWUGEo1x7">
-              Submit punishments{" "}
-            </a>{" "}
-          </li>
-          <li>
-            {" "}
-            <a href="https://docs.google.com/spreadsheets/d/1HqBUGAbqIflPSKtJ93eNfqBkUkCzmMyVnv0PcwvXNFw/edit?usp=sharing">
+        <footer className={styles.footer}>
+          <h2 className={styles.description}>Links:</h2>
+          <ul>
+            <li>
               {" "}
-              Keepers{" "}
-            </a>{" "}
-          </li>
-        </ul>
-      </footer>
-    </div>
+              <a href="https://forms.gle/hEWCH8fZkWUGEo1x7">
+                Submit punishments{" "}
+              </a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="https://docs.google.com/spreadsheets/d/1HqBUGAbqIflPSKtJ93eNfqBkUkCzmMyVnv0PcwvXNFw/edit?usp=sharing">
+                {" "}
+                Keepers{" "}
+              </a>{" "}
+            </li>
+          </ul>
+        </footer>
+      </div>
+    </>
   );
 }
